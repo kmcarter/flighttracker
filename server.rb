fa5b67f817f8ac49b4ca88c5d6d42fea47233555
kmc4637 <kmc4637@gmail.com>
2015-12-03 22:11:14 -0500

    Make server object-oriented

--- server.rb
+++ server.rb
@@ -1,17 +1,40 @@
 require "socket"
-flight_server = TCPServer.new('localhost', 80)
-sockaddr = flight_server.addr
-puts "Echo server running on #{sockaddr.join(':')}"
+require "json"
+require 'byebug'
+load 'controller.rb'
 
-loop do
-  Thread.start(flight_server.accept) do | sock |
-    puts("#{sock} connected at #{Time.now}")
+class FlightServer
+  
+  def initialize host = 'localhost', port = 80
+    @host = host
+    @port = port
+    @controller = FlightController.new
+  end
+  
+  def start
+    @flight_server = TCPServer.new(@host, @port)
+    sockaddr = @flight_server.addr
+    puts "Echo server running on #{sockaddr.join(':')}"
+
     loop do
-      input = sock.gets
-      sock.write(input)
-      puts "User entered: #{input}"
+      Thread.start(@flight_server.accept) do | sock |
+        puts "#{sock} connected at #{Time.now}"
+        sock.write(current_flights_to_json)
+        puts "#{sock} disconnected at #{Time.now}"
+        sock.close
+      end
     end
-    puts("#{sock} disconnected at #{Time.now}")
-    sock.close
+  end
+  
+  def stop
+    @flight_server.close
+  end
+  
+  def current_flights_to_json
+    flights = @controller.current_flights
+    JSON.generate(flights)
   end
 end
+
+#srv = FlightServer.new('localhost', '1024')
+#srv.start
