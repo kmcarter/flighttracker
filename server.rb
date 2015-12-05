04c7c5f0ad7281a9aad7e20b0b3b1de4441444f3
Kelly Carter <kmc4637@gmail.com>
2015-12-05 16:32:58 -0500

    Working server!

--- server.rb
+++ server.rb
@@ -4,23 +4,32 @@ require 'byebug'
 load 'controller.rb'
 
 class FlightServer
+  CONTENT_TYPE_MAPPING = {
+    'html' => 'text/html',
+    'json' => 'application/json',
+    'js' => 'application/javascript',
+    'css' => 'text/css',
+    'png' => 'image/png',
+    'jpg' => 'image/jpeg'
+  }
+  DEFAULT_CONTENT_TYPE = 'application/octet-stream'
+  WEB_ROOT = 'public'
   
-  def initialize host = 'localhost', port = 80
-    @host = host
+  def initialize port = 80
     @port = port
     @controller = FlightController.new
   end
   
   def start
-    @flight_server = TCPServer.new(@host, @port)
+    @flight_server = TCPServer.open(@port)
     sockaddr = @flight_server.addr
-    puts "Echo server running on #{sockaddr.join(':')}"
+    puts "Flight control server running on #{sockaddr.join(':')}"
 
     loop do
       Thread.start(@flight_server.accept) do | sock |
-        puts "#{sock} connected at #{Time.now}"
-        sock.write(current_flights_to_json)
-        puts "#{sock} disconnected at #{Time.now}"
+        path = sock.gets.split[1]
+        p "Request for " + path
+        serve_request(path, sock)
         sock.close
       end
     end
@@ -30,11 +39,57 @@ class FlightServer
     @flight_server.close
   end
   
+  def serve_request rel_path, socket
+    #path_segments = rel_path.split('/')
+    rel_path = '/index.html' if rel_path == '/'
+    path = WEB_ROOT + rel_path
+    
+    if rel_path.start_with? '/entry'
+      "Creating flight from params"
+    elsif rel_path.start_with? '/tracking_info'
+      current_flights_to_json
+    else
+      if File.exist?(path) && !File.directory?(path)
+        File.open(path, 'rb') do |file|
+          socket.print "HTTP/1.1 200 OK\r\n" +
+                       "Content-Type: #{content_type(file)}\r\n" +
+                       "Content-Length: #{file.size}\r\n" +
+                       "Connection: close\r\n"
+
+          socket.print "\r\n"
+
+          # write the contents of the file to the socket
+          IO.copy_stream(file, socket)
+        end
+      else
+        message = "File not found\n"
+        print message
+
+        # respond with a 404 error code to indicate the file does not exist
+        socket.print "HTTP/1.1 404 Not Found\r\n" +
+                     "Content-Type: text/plain\r\n" +
+                     "Content-Length: #{message.size}\r\n" +
+                     "Connection: close\r\n"
+
+        socket.print "\r\n"
+
+        socket.print message
+      end
+    end
+  end
+  
+  def content_type(path)
+    ext = File.extname(path).split(".").last
+    CONTENT_TYPE_MAPPING.fetch(ext, DEFAULT_CONTENT_TYPE)
+  end
+  
   def current_flights_to_json
-    flights = @controller.current_flights
+    flights = FlightController.airborne_flights.map do | flight |
+      { id: flight.id, flight_num: flight.flight_num, speed: flight.speed, altitude: flight.current_altitude }
+    end
     JSON.generate(flights)
   end
 end
 
-#srv = FlightServer.new('localhost', '1024')
-#srv.start
+srv = FlightServer.new(3000)
+srv.start
